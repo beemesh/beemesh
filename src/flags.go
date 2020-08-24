91764650ad9ad66dd12ed34c593a576ef805e6ba
Victor Kamenov <victor.kamenov1@swisscom.com>
2020-08-24 21:33:35 +0200

    src/flags.go - do not use pointer receivers on addrList Set and String methods - underlying type is a reference type

--- src/flags.go
+++ src/flags.go
@@ -11,20 +11,20 @@ import (
 // A new type we need for writing a custom flag parser
 type addrList []maddr.Multiaddr
 
-func (al *addrList) String() string {
-	strs := make([]string, len(*al))
-	for i, addr := range *al {
+func (al addrList) String() string {
+	strs := make([]string, len(al))
+	for i, addr := range al {
 		strs[i] = addr.String()
 	}
 	return strings.Join(strs, ",")
 }
 
-func (al *addrList) Set(value string) error {
+func (al addrList) Set(value string) error {
 	addr, err := maddr.NewMultiaddr(value)
 	if err != nil {
 		return err
 	}
-	*al = append(*al, addr)
+	al = append(al, addr)
 	return nil
 }
 
