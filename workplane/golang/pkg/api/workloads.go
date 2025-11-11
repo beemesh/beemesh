3277f01b2ecb37b8abe428643c7148944f93e95d
flavioaiello <11198273+flavioaiello@users.noreply.github.com>
2025-11-11 20:16:20 +0100

    Remove Golang workplane implementation

--- workplane/golang/pkg/api/workloads.go
+++
@@ -1,8 +0,0 @@
-package api
-
-type WorkloadSpec struct {
-	Name     string            `json:"name"`
-	Image    string            `json:"image"`
-	Replicas int               `json:"replicas"`
-	Env      map[string]string `json:"env,omitempty"`
-}
